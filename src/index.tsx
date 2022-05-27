98a5ec142d7c13dff4fea477e1623c278b78136e
Assaad El Halabi <asaadalhalabi@gmail.com>
2022-05-27 22:56:52 +0300

    Some Fixes

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,13 @@ import ReactDOM from "react-dom";
 
 
 const App = () => {
-    
-    return(
+
+    return (
         <div>
             <h1>Hi there</h1>
         </div>
-    )
-}
+    );
+};
 
 
 ReactDOM.render(<App />, document.querySelector('#root'));
