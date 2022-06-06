1ba9bfb3afeff34b4f4c3f913399093e4774b6a2
Assaad El Halabi <asaadalhalabi@gmail.com>
2022-06-06 04:54:04 +0300

    Adding ESBuild For Transpiling And Bundling

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,53 @@
-import React from "react";
+import * as esbuild from 'esbuild-wasm';
+import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./components/App";
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
+const App = () => {
+
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
+    const ref = useRef<boolean>();
+
+    const startService = async () => {
+        await esbuild.initialize({
+          worker: true,
+          wasmURL: '/esbuild.wasm'
+        })
+        ref.current = true
+      }
+      
+      useEffect(() => {
+        startService()
+        console.log('hey');
+        
+      }, [])
+      
+
+    const onClick = async () => {
+        if (!ref.current) {
+            return;
+          }
+          const result = await esbuild.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin()]
+          })
+        //   console.log(result);
+          setCode(result.outputFiles[0].text)
+        
+    };
+
+    return <div>
+        <textarea value={input} onChange={e => setInput(e.target.value)} name="code cell" id="code cell 1" placeholder="Code" cols={30} rows={10}></textarea>
+        <div>
+            <button onClick={onClick} type="button">Submit</button>
+        </div>
+        <pre>{code}</pre>
+    </div>;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-    <React.StrictMode> <App /> </React.StrictMode>);
+     <App />);
