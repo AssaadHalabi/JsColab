e43cbf30f16712f65a43cf74ef896eaec9b5704c
AssaadHalabi <asaadalhalabi@gmail.com>
2022-06-09 13:33:23 +0300

    Action Creators Implementations

--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -1,12 +1,10 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
 import cellsReducer from './cellsReducer';
 
-
-
 const reducers = combineReducers({
-    cells: cellsReducer,
+  cells: cellsReducer,
 });
 
 export default reducers;
 
-export type RootState = ReturnType<typeof reducers>
+export type RootState = ReturnType<typeof reducers>;
