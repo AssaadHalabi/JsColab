cf54b1d4107e252ba4d54020784d7b33a94c28b3
AssaadHalabi <asaadalhalabi@gmail.com>
2022-06-22 02:56:43 +0300

    Remove Ununsed Secret Env Variables

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -15,6 +15,6 @@ app.use(bodyParser.json({ type: "*/*" }));
 
 router(app);
 
-app.listen(4000, () => {
-  console.log(`Server started on port  + ${4000}`);
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`Server started on port  + ${process.env.PORT || 4000}`);
 });
